75abfea66408eac265bf00faa02b44ca5d3738d4
moliver226 <marcusoliver226@gmail.com>
2022-10-25 15:06:37 -0700

    Project files updated.

--- weather_tracker/weather_tracker/settings.py
+++ weather_tracker/weather_tracker/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = 'django-insecure-@xpidl_=x_1ho)740v(q46qfrd_vl*)fo_^s8&%i7ply#*$cy7
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
